d4581c6ffee32a6e9a41616469fa0b44cefdf22f
mJordan <hi@mjordan.codes>
2018-10-10 10:29:00 -0700

    pulled out community involvement into own file

---
+++ src/sections/community.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { StaticQuery, graphql } from "gatsby"
+
+import SubOne from '../components/subone'
+import Community from '../components/community'
+
+
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query CommunityQuery {
+        contentfulAboutTopic(
+          tItle:{
+            eq:"Community Involvement"
+          }
+        ) {
+          description {
+            description
+          }
+          about_sub_topic {
+            id
+            title
+            description {
+              description
+            }
+            logo {
+              file {
+                url
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+      <SubOne 
+        title="Community Involvement"
+        isFlex={false}>
+        {data.contentfulAboutTopic.description.description}
+      </SubOne>
+      <SubOne
+        isFlex={true}>
+        {data.contentfulAboutTopic.about_sub_topic.map(edge => (
+          <Community
+            title={edge.title}
+            image={edge.logo.file.url}
+            children={edge.description.description}
+          />
+        ))}
+       
+        
+      </SubOne>
+      </>
+    )}
+  />
+)
+
