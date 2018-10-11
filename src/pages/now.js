618eb40fb00f7001c6fbd565f54c23f39f1cf275
mJordan <hi@mjordan.codes>
2018-10-11 10:10:21 -0700

    adding now page

---
+++ src/pages/now.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const NowPage = () => (
+  <Layout>
+    <div>
+      Now
+    </div>
+  </Layout>
+)
+
+export default NowPage
